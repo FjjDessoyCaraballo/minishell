097d1d51bf88e0526aeb17d93b8a8d50382daa06
Torutu <waleedalnaimi117@gmail.com>
2024-07-10 11:46:10 +0300

    small change in ft_buildin_check

--- src/parse/tokenizer.c
+++ src/parse/tokenizer.c
@@ -4,23 +4,23 @@
 void	ft_builtin_check(char* token)
 {
 	if (strcmp(token,"echo") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token,"cd") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token,"pwd") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token,"export") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token,"unset") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token, "env") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token, "exit") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else if (strcmp(token, "\"") == 0)
-		printf("token:%s\n", token);
+		printf("token:[%s]\n", token);
 	else
-		printf("Unknown command :(%s)\n", token);
+		printf("Unknown command:[%s]\n", token);
 }
 
 int		ft_charinstr(char c,const char *str)
