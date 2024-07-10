235800e6d1e94e45a6af0a5b0b09212698d15513
Torutu <waleedalnaimi117@gmail.com>
2024-07-10 09:27:45 +0300

    the start of line tokenization

--- src/line_handler.c
+++ src/line_handler.c
@@ -10,14 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
 
-// readline malloc's a new buffer every time.
 int	sniff_line(t_data *data)
 {
 	data->line_read = readline("[I can't believe this is not shell] ");
 	if (!data->line_read)
 		return (0);
+	line_tokenization(data->line_read);
 	// line_parsing(data->line_read); // LEO'S PART
 	if (data->line_read || *data->line_read)
 		add_history(data->line_read);
