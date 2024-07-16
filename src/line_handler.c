ba3a53549cc499d941f59ccb0ad9ab13ca8e9655
Felipe Dessoy Caraballo <fdessoycaraballo@gmail.com>
2024-07-16 13:56:32 +0300

    putting some color for us

--- src/line_handler.c
+++ src/line_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   line_handler.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdessoy- <fdessoy-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fdessoy- <fdessoy-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:23:49 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/05/23 13:37:45 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2024/07/16 13:56:20 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 int	sniff_line(t_data *data)
 {
-	data->line_read = readline("[I can't believe this is not shell] ");
+	data->line_read = readline("\e[45m[I can't believe this is not shell] \e[0m");
 	if (!data->line_read)
 		return (0);
 	line_tokenization(data);
-	// line_parsing(data->line_read); // LEO'S PART
 	if (data->line_read || *data->line_read)
 		add_history(data->line_read);
 	return (1);
