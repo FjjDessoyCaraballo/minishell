963b591a46197c8f99140ce34e585537ae8def65
Torutu <waleedalnaimi117@gmail.com>
2024-07-30 12:46:42 +0300

    adjusment

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,13 +6,13 @@
 /*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 11:49:16 by walnaimi          #+#    #+#             */
-/*   Updated: 2024/07/29 22:53:27 by walnaimi         ###   ########.fr       */
+/*   Updated: 2024/07/30 12:40:57 by walnaimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void enter_file(int fd)
+void enter_file(int fd)// this is what happens inside the file as we add texts to it
 {
     char buffer[1024];
     ssize_t bytes_read;
@@ -28,7 +28,7 @@ void enter_file(int fd)
 
         buffer[bytes_read] = '\0'; // Null-terminate the string
 
-        if (strncmp(buffer, "END", 3) == 0 && (buffer[3] == '\n' || buffer[3] == '\0'))
+        if (strncmp(buffer, "END", 3) == 0 && (buffer[3] == '\n' || buffer[3] == '\0'))//if it finds END it breaks and goes to close
         {
             break;
         }
@@ -40,24 +40,24 @@ void enter_file(int fd)
         }
     }
 
-    close(fd);
+    close(fd);//closes file ofc
 }
 
 int handle_output_redirection(t_token *current_token, t_token *next_token)
 {
-    int fd;
+    int fd;//using int fd so the append knows where to add the new text
 
     if (current_token->type == RED_OUT)
     {
         fd = open(next_token->value, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-        if (fd < 0)
+        if (fd < 0)//can be removed perhaps??? but better to stay safe than sorry
         {
             perror("open");
             return FAILURE;
         }
-        enter_file(fd);
-        printf("%d\n",fd);
-        //printf("\n");
+        enter_file(fd);// once we got fd open and ready we enter the file to write
+        //printf("%d\n",fd);debug
+        //printf("\n");perhaps not needed?
         return SUCCESS;
     }
     else if (current_token->type == APPEND)
@@ -69,7 +69,7 @@ int handle_output_redirection(t_token *current_token, t_token *next_token)
             return FAILURE;
         }
         enter_file(fd);
-        printf("%d\n",fd);
+        //printf("%d\n",fd);debug
         //printf("\n");
         return SUCCESS;
     }
