11985852b55ac9259ae8910948d12867b86c0798
Bruno Posa <bposa@student.hive.fi>
2024-08-19 12:53:59 +0300

    normed - init.c

--- src/init.c
+++ src/init.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: bposa <bposa@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:38:16 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/08/07 00:53:19 by walnaimi         ###   ########.fr       */
+/*   Updated: 2024/08/19 12:51:53 by bposa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-// node is getting a copy of env in a linked list
+
 void	ll_env(t_env **env_ll, char **env)
 {
 	t_env	*tmp;
@@ -64,10 +64,9 @@ void	initializer(t_data *data, t_env **env_ll, char **env)
 	ll_env(env_ll, env);
 	find_bin(env_ll, data);
 	data->binary_paths = ft_split(data->bin, ':');
-	data->envll = *env_ll; //to make sure the data->envll points to the initialized env_ll
+	data->envll = *env_ll;
 	data->in_quotes = 0;
 	data->echoed = false;
-	//print_binary_paths(data);
 	data->echoed = false;
 	data->cmd_ignore = false;
 }
