2c70b5a8cfb31a6420df3973f0f222da6246d6df
Felipe Dessoy Caraballo <fdessoycaraballo@gmail.com>
2024-08-19 15:59:44 +0300

    updating heredoc

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdessoy- <fdessoy-@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: fdessoy- <fdessoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 13:03:21 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/08/14 10:06:07 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2024/08/19 15:54:26 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,20 @@ void redirections_handling(t_data *data, char **array)
 			if (array[data->index + 1])
 			{
 				here_doc(data, array[data->index + 1]);
-				dup2(data->pipe_fd[0], STDIN_FILENO);
-				close(data->pipe_fd[0]);
-				close(data->pipe_fd[1]);
+				if (data->piped == true)
+				{
+					dup2(data->pipe_fd[0], STDIN_FILENO);
+					close(data->pipe_fd[0]);
+					close(data->pipe_fd[1]);
+				}
+				else
+				{
+					data->fd_in = open("/tmp/heredoc_tmp", O_RDONLY);
+					if (data->fd_in < 0)
+						exit(err_msg(NULL, HEREDOC_FAILURE, 1));
+					dup2(data->fd_in, STDIN_FILENO);
+					close(data->fd_in);
+				}
 			}
 			else
 				exit(err_msg("'newline'", SYNTAX, 2));		
@@ -75,14 +86,31 @@ void here_doc(t_data *data, char *delimiter)
 {
 	char	*input;
 
+	if (data->piped == false)
+	{
+		data->fd_in = open("/tmp/heredoc_tmp", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (data->fd_in < 0)
+			exit(err_msg(NULL, HEREDOC_FAILURE, 1));
+	}
 	while (1)
 	{
 		input = readline("8==D ");
 		if (!ft_strncmp(input, delimiter, ft_strlen(delimiter)))
 			break ;
-		write(data->pipe_fd[1], input, ft_strlen(input));
-		write(data->pipe_fd[1], "\n", 1);
+		if (data->piped == true)
+		{
+			write(data->pipe_fd[1], input, ft_strlen(input));
+			write(data->pipe_fd[1], "\n", 1);
+		}
+		else
+		{			
+			write(data->fd_in, input, ft_strlen(input));
+			write(data->fd_in, "\n", ft_strlen(input));
+		}
+		free(input);
 	}
+	if (data->piped == false)
+		close(data->fd_in);
 }
 
 int	find_redirection(char **array)
