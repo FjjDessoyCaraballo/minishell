1d0852f8bcf2df0031bb047e7d7cd0b1a99cb071
Felipe Dessoy Caraballo <fdessoycaraballo@gmail.com>
2024-08-19 15:43:23 +0300

    updating to change branch

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -6,7 +6,7 @@
 /*   By: fdessoy- <fdessoy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 13:03:21 by fdessoy-          #+#    #+#             */
-/*   Updated: 2024/08/19 15:34:19 by fdessoy-         ###   ########.fr       */
+/*   Updated: 2024/08/19 15:42:22 by fdessoy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,31 @@ void here_doc(t_data *data, char *delimiter)
 {
 	char	*input;
 
+	if (data->piped == false)
+	{
+		data->fd_in = open("/tmp/heredoc_tmp", O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (data->fd_in < 0)
+			exit(err_msg(NULL, HEREDOC_FAILURE, 1));
+	}
 	while (1)
 	{
 		input = readline("8==D ");
 		if (!ft_strncmp(input, delimiter, ft_strlen(delimiter)))
 			break ;
-		write(data->pipe_fd[1], input, ft_strlen(input));
-		write(data->pipe_fd[1], "\n", 1);
+		if (data->piped == true)
+		{
+			write(data->pipe_fd[1], input, ft_strlen(input));
+			write(data->pipe_fd[1], "\n", 1);
+		}
+		else
+		{			
+			write(data->fd_in, input, ft_strlen(input));
+			write(data->fd_in, "\n", ft_strlen(input));
+		}
+		free(input);
 	}
+	if (data->piped == false)
+		close(data->fd_in);
 }
 
 int	find_redirection(char **array)
